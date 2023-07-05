74c344719dbd8d45c3bb6b1fdb3341b00ca09819
mohammadrostamiorg <mohammad.jayant@gmail.com>
2023-07-05 15:41:47 +0330

    add delete character route to api.php file

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Panel\CharactersController;
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
+|--------------------------------------------------------------------------|
+| API Routes                                                                                                              |
+|--------------------------------------------------------------------------|
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
