0b5a758a4ab4d079c9fd15f0c53703965adad68d
mohammadrostamiorg <mohammad.jayant@gmail.com>
2023-07-18 21:47:22 +0330

    add houses panel viewes - copied of characters viewes

---
+++ resources/views/Panel/houses/all.blade.php
@@ -0,0 +1,72 @@
+@extends('Panel.layout.master')
+
+
+@section('content')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+
+        }
+
+        .container {
+            width: 100vw;
+            min-height: 50lvh;
+            background: #191C24;
+            margin: 100px auto;
+            direction: rtl;
+            box-sizing: border-box;
+            padding: 50px;
+
+        }
+
+        .container-fluid {
+            padding: 0;
+        }
+
+        .container .row {
+            width: 90%;
+            background: #000000;
+            margin: 30px auto;
+            padding: 50px;
+            border-radius: 25px;
+            color: #fff;
+            text-align: center;
+        }
+
+        .container .row p {
+            text-align: center;
+            font-size: 2em;
+        }
+    </style>
+    <div class="container">
+        @foreach($Characters as $Character)
+            <div class="row">
+                <p>
+                    {{ $Character->name }}
+                </p>
+                <div class="btns">
+
+                    <form method="post" action="{{ url('/api/characters/delete/'.$Character->id) }}">
+                        @method('delete')
+                        @csrf
+                        <button class="btn btn-danger">
+                Delete
+            </button>
+                    </form>
+
+                    </a>
+                    <a class="block btn-link" href="{{ url('panel/characters/edit/'.$Character->id) }}">
+                    <span class="btn btn-warning">
+
+                            Edit
+                    </span>
+                    </a>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
+@endsection
---
+++ resources/views/Panel/houses/new.blade.php
@@ -0,0 +1,177 @@
+@extends('Panel.layout.master')
+
+
+@section('content')
+    <style>
+        .container{
+            width: 85lvw;
+            min-height: 50lvh;
+            background: #191C24;
+            margin: 100px auto;
+            direction: rtl;
+            box-sizing: border-box;
+            padding: 50px;
+        }
+        .container .row{
+            display: flex;
+            flex-direction:column ;
+        }
+        .container input , .container select , .container textarea{
+            width: 80%;
+            margin: 30px;
+            height: 50px;
+            color: #fff;
+            background: rgba(0,0,0,.5);
+            border: none;
+
+        }
+        .container label{
+            display: block;
+            direction: rtl;
+            text-align: right;
+
+        }
+        .container textarea{
+            height: 100px;
+        }
+    </style>
+    <div class="container">
+        <form enctype="multipart/form-data" method="post" action="{{route('character.store')}}">
+            @csrf
+
+            <div class="row">
+                <label style="display: block">
+                    اسم :
+                </label>
+                <br>
+                <br>
+                <Input name="name" type="text" placeholder="اسم کارکتر را وارد کنید">
+            </div>
+            <div class="row">
+                <label style="display: block">
+                    عکس :
+                </label>
+                <br>
+                <br>
+                <Input name="pic" type="file">
+            </div>
+            <div class="row">
+                <label style="display: block">
+                    پدر :
+                </label>
+                <br>
+                <br>
+                <Input name="father" type="text" placeholder="اسم پدر کارکتر را وارد کنید">
+            </div>
+            <div class="row">
+                <label style="display: block">
+                    مادر :
+                </label>
+                <br>
+                <br>
+                <Input name="mother"  type="text" placeholder="اسم مادر کارکتر را وارد کنید">
+            </div>
+            <div class="row">
+                <label style="display: block">
+                    خاندان :
+                </label>
+                <br>
+                <br>
+                <select name="house" >
+{{--                    this secition will be dynamic later --}}
+                    <option>لنیستر</option>
+                    <option>استارک</option>
+                    <option>تارگرین</option>
+                    <option>باراتیون</option>
+                    <option>گریجوی</option>
+                    <option>تالی</option>
+                </select>
+            </div>
+
+            <div class="row">
+                <label style="display: block">
+                    لقب ها(لقب هارا با کاما , از هم جدا کنید) :
+                </label>
+                <br>
+                <br>
+                <textarea name="aliases" >
+
+                </textarea>
+            </div>
+
+
+            <div class="row">
+                <label style="display: block">
+                    تولد :
+                </label>
+                <br>
+                <br>
+                <Input name="born"  type="text" placeholder="سال تولد بر اساس فتح اگان">
+            </div>
+
+
+            <div class="row">
+                <label style="display: block">
+                    مرگ :
+                </label>
+                <br>
+                <br>
+                <Input name="died" type="text" placeholder="سال مردن بر اساس فتح اگان">
+            </div>
+            <div class="row">
+                <label style="display: block">
+                    جنسیت  :
+                </label>
+                <br>
+                <br>
+                <select name="gender" >
+                    <option value="male">
+                        مذکر
+                    </option>
+                    <option value="female">
+                        مونث
+                    </option>
+                </select>
+            </div>
+
+            <div class="row">
+                <label style="display: block">
+                    فرزند ها(فرزند هارا با کاما , از هم جدا کنید) :
+                </label>
+                <br>
+                <br>
+                <textarea name="children">
+
+                </textarea>
+            </div>
+
+            <div class="row">
+                <label style="display: block">
+                    بازیگر :
+                </label>
+                <br>
+                <br>
+                <Input name="playedBy" type="text" placeholder="چه کسی نقش اورا ایفا کرده">
+            </div>
+            <div class="row">
+                <label style="display: block">
+                    متن معرفی کاراکتر
+                </label>
+                <br>
+                <br>
+                <textarea name="content" style="min-height: 500px">
+
+                </textarea>
+            </div>
+
+
+            <div class="row">
+                <button style="background: #00ac4a;width: 100px;height: 50px;border-radius:30px;box-sizing: border-box;padding: 5px;text-align: center;color: #fff;border: none ">
+                    ثبت کاراکتر
+                </button>
+            </div>
+
+        </form>
+    </div>
+
+@endsection
